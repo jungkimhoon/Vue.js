d1847df64c2ac921be8bd5117dbd449df577d25b
12345 <ing2895@naver.com>
2020-06-17 11:03:24 +0900

    vue_quiz2

---
+++ WebContent/vue2_quiz/js/app.js
@@ -0,0 +1,20 @@
+
+
+Vue.component('sibling-component',{
+	props: ['propsdata'],
+	template: '<p>{{propsdata}}</p>'
+});
+
+Vue.component('child-component',{
+	props: ['propsdata'],
+	template: '<p>{{propsdata}}</p>'
+});
+
+var app = new Vue({
+	el: '#app',
+	data:{
+		message: 'Hello Vue! passed from Parent Component',
+		anotherMessage: '돼냐 이거'
+	}	
+}); 
+
